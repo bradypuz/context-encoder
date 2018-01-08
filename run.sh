4369327dd4e7855d01f75400bd0a437c7b51ebed
bradypuz <bradypus@hotmail.com>
2018-01-08 15:27:57 -0500

    modify for 128 res

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 # !/bin/bash 
-python train.py --dataset folder --dataroot /home/snie/Desktop/celeba_1024_ce/train --cuda --wtl2 0.999 --niter 500 --outf result/celeba1024_v1 --imageSize 1024
+python train.py --dataset folder --dataroot /media/zeyuan/ACD207B0D2077DB8/dataset/celeba_256_ce/train --cuda --wtl2 0.999 --niter 500 --outf result/celeba128_v1 --imageSize 128
